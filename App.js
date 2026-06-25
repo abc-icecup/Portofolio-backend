ba1619b301e98bd1775351ef7d83272e7d60b303
Ratna Ruspita Dewi <ratnadhewi6@gmail.com>
2026-06-25 07:05:25 +0700

    persiapan ke vercel

--- App.js
+++ App.js
@@ -50,8 +50,12 @@ sequelize.authenticate()
 //   .then(() => console.log("Table created"))
 //   .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
