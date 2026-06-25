a7c57b46f3777a832e761a2a39ce8b08f769da3e
Ratna Ruspita Dewi <ratnadhewi6@gmail.com>
2026-06-25 16:47:04 +0700

    persiapan ke railway

--- App.js
+++ App.js
@@ -50,12 +50,8 @@ sequelize.authenticate()
 //   .then(() => console.log("Table created"))
 //   .catch(err => console.log(err));
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
