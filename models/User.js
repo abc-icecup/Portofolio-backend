04696c88fe26bb613642e4fda7e6c5b23b7b0c44
Jumrianti <jumrianti@email.com>
2026-06-26 13:58:50 +0800

    debug error tabel users

--- models/User.js
+++ models/User.js
@@ -2,16 +2,22 @@ import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
 import Certificate from "./certificates.js";
 
-const User = sequelize.define("User", {
-  username: {
-    type: DataTypes.STRING,
+const User = sequelize.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
   },
-  email: {
-    type: DataTypes.STRING,
-  },
-  password: {
-    type: DataTypes.STRING,
-  },
-});
+  {
+    tableName: "users",
+  }
+);
 
 export default User;
